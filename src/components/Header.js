073945230b22aec7af2a7b8c35dfadf1e814cd80
vyllian <anastasiia.vylitkova.kn.2021@lpnu.ua>
2023-06-03 00:51:01 +0300

    header started

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Logo from '../media/logo.svg'
+
+class Header extends React.Component{
+    render(){
+        return(
+            <header>
+                <img src={Logo} alt='logo'></img>
+                <div className='navigation'>
+                    <h3 className='menu-item active'>Home</h3>
+                    <h3 className='menu-item'>Inspiration</h3>
+                    <h3 className='menu-item'>Create</h3>
+                </div>
+            </header>
+        )
+    }
+}
+
+export default Header
