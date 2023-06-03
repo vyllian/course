c967f8d2e93ad2ec98ecf1637ec9b82612f53b03
vyllian <anastasiia.vylitkova.kn.2021@lpnu.ua>
2023-06-03 12:39:32 +0300

    started footer

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import logo from '../media/logo.svg'
+
+class Footer extends React.Component{
+    render(){
+        return(
+            <footer>
+                <div>
+                    
+                </div>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+            </footer>
+        )
+    }
+}
+
+export default Footer
