b09715fda3cb49ddc22af96b64c79625477f19b2
vyllian <anastasiia.vylitkova.kn.2021@lpnu.ua>
2023-06-03 12:39:11 +0300

    added button

--- src/App.js
+++ src/App.js
@@ -4,7 +4,18 @@ import Header from "./components/Header";
 class App extends React.Component{
     render(){
         return(
-            <Header/>
+            <div className="main-page">
+                <Header/>
+                <h1>NEVIL</h1>
+                <div className="container">
+                    <p>is a resource with clothes for clothes.
+Here you can create outfits using our database with items from different brands. Train your styling skills, find needed clothes for wardrobe or just have fun with us!</p>
+                    <div className="button-container">
+                        <button className="main-button">Create Outfit</button>
+                    </div>
+                    
+                </div>
+            </div>
         )
     }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import Logo from '../media/logo.svg'
+import logo from '../media/logo.svg'
 
 class Header extends React.Component{
     render(){
         return(
             <header>
-                <img  src={Logo} alt='logo'></img>
+                <img src={logo} alt='logo'></img>
                 <div className='navigation'>
                     <h3 className='menu-item active'>Home</h3>
                     <h3 className='menu-item'>Inspiration</h3>
