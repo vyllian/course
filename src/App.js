682bdbc4861d6d786b7bd12dfc7c5a64a12a0955
vyllian <anastasiia.vylitkova.kn.2021@lpnu.ua>
2023-06-04 15:10:14 +0300

    finished footer

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from "react";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 class App extends React.Component{
     render(){
         return(
-            <div className="main-page">
+            <div>
                 <Header/>
-                <h1>NEVIL</h1>
-                <div className="container">
-                    <p>is a resource with clothes for clothes.
-Here you can create outfits using our database with items from different brands. Train your styling skills, find needed clothes for wardrobe or just have fun with us!</p>
-                    <div className="button-container">
-                        <button className="main-button">Create Outfit</button>
+                <div className="main-page page">
+                    <h1>NEVIL</h1>
+                    <div className="container">
+                        <p className="info">is a resource with clothes for clothes.
+    Here you can create outfits using our database with items from different brands. Train your styling skills, find needed clothes for wardrobe or just have fun with us!</p>
+                        <div className="button-container">
+                            <button className="main-button">Create Outfit</button>
+                        </div>
                     </div>
-                    
                 </div>
+                <Footer/>
             </div>
         )
     }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,38 @@
 import React from 'react'
 import logo from '../media/logo.svg'
+import fbook from '../media/socials/Facebook - Negative.svg'
+import insta from '../media/socials/Instagram - Negative.svg'
+import pint from '../media/socials/Pinterest - Negative.svg'
+import twit from '../media/socials/Twitter - Negative.svg'
+import lines from '../media/element2.svg'
+
 
 class Footer extends React.Component{
     render(){
         return(
             <footer>
-                <div>
-                    
+                <div className='add'>
+                    <img className='element' src={lines} alt='lines'></img>
                 </div>
-                <div>
-
-                </div>
-                <div>
-
+                <div className='main-footer'>
+                    <div className='container'>
+                        <img className='logo' src={logo} alt='logo'></img>
+                        <div className='socials' >
+                            <img src={fbook} alt='faceboook'></img>
+                            <img src={insta} alt='instagram'></img>
+                            <img src={pint} alt='pinterest'></img>
+                            <img src={twit} alt='twitter'></img>
+                        </div>
+                    </div>
+                    <div className='navigation'>
+                        <h3 className='active-footer'>Home</h3>
+                        <h3>Inspiration</h3>
+                        <h3>Create</h3>
+                    </div>
+                    <div className='container'>
+                        <p className='nevil-org'>Contact us:<br/>nevil.org@gmail.com</p>
+                        <p className='nevil-org'>©All right Reversed.<br/>Nevil Corporation 2023</p>
+                    </div>
                 </div>
             </footer>
         )
