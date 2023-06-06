c3cf83b97665aa8790624208a60536011617135e
vyllian <anastasiia.vylitkova.kn.2021@lpnu.ua>
2023-06-07 02:02:18 +0300

    added some styles for insparation page

--- src/pages/Inspiration.js
+++ src/pages/Inspiration.js
@@ -9,6 +9,59 @@ class Inspiration extends React.Component{
                 <div className="noise"></div>
                 <Header/>
                 <div className="inspiration-page page">
+                    <div className="header">
+                        <h1>ALL</h1>
+                        <h1>AWESOME OUTFITS</h1>
+                    </div>
+                    
+                    <div className="filters-container">
+                        <div className="properties">
+                            
+                            <select name="style" id="style">
+                                <option selected hidden>Style</option>
+                                <option value="casual">Casual</option>
+                                <option value="sport">Sport</option>
+                                <option value="formal">Formal</option>
+                                <option value="vintage">Vintage</option>
+                                <option value="grunge">Grunge</option>
+                                <option value="chic">Chic</option>
+                                <option value="bohemian">Bohemian</option>
+                                <option value="preppy">Preppy</option>
+                                <option value="tomboy">Tomboy</option>
+                            </select>
+                            <select name="mood" id="mood">
+                                <option selected hidden>Mood</option>
+                                <option value="sportic">Sportic</option>
+                                <option value="relaxing">Relaxing</option>
+                                <option value="interesting">Interesting</option>
+                                <option value="sexy">Sexy</option>
+                                <option value="sad">Sad</option>
+                                <option value="cheerful">Cheerful</option>
+                            </select>
+                            <select name="season" id="season">
+                                <option selected hidden>Season</option>
+                                <option value="winter">Winter</option>
+                                <option value="Spring">Spring</option>
+                                <option value="Summer">Summer</option>
+                                <option value="Autumn">Autumn</option>
+                            </select>
+                        </div>
+                        <div className="colors">
+                            
+                        </div>
+                    </div>
+                    <div className="inspo-outfit-container">
+                        <div className="img-block">
+                            <img src="" alt="outfit-image"></img>
+                        </div>
+                        <div className="outfit-description">
+                            <p>time</p>
+                            <div className="likes">
+                                <p></p>
+                                <img></img>
+                            </div>
+                        </div>
+                    </div>
                    
                 </div>
                 <Footer/>
