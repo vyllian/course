807f0f9b79a0720a56800f6153e0ddce49f13533
vyllian <anastasiia.vylitkova.kn.2021@lpnu.ua>
2023-06-07 19:29:45 +0300

    added main elements to the page

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import download from "../media/download.svg"
+
 
 class Create extends React.Component{
     render(){
@@ -9,14 +11,32 @@ class Create extends React.Component{
                 <div className="noise"></div>
                 <Header/>
                 <div className="create-page page">
-                   <h1></h1>
-                   <div className="creating-template">
+                    <div className="header-pow">
+                        <h1>ADD CLOTHES TO THE TEMPLATE</h1>
+                        <h1 id="ques-mark"><small><a href="#tutor">?</a></small></h1>
+                    </div>
+                   <div className="creating-container">
+                        <div className="creating-template">
 
+                        </div>
+                        <div className="template-bar">
+                            <button>Publish</button>
+                            <button>
+                                <img src={download} alt="download"></img>
+                            </button>
+                        </div>
                    </div>
-                   <h1></h1>
-                   <li>
-                    
-                   </li>
+                   <h1 id="tutor">HOW TO MAKE AN OUTFIT</h1>
+                   <ol>
+                        <li>Go to the template above</li>
+                        <li>Press “+” near the element you want to add</li>
+                        <li>Choose type and/or color in this category</li>
+                        <li>Take a look on offered clothes, find desirable item</li>
+                        <li>Add more & express yourself</li>
+                        <li>When your outfit is done press “Publish” button to add the image to the Inspiration page</li>
+                        <li>Download image to your computer</li>
+                        <li>Start over again and create another one!</li>
+                   </ol>
                 </div>
                 <Footer/>
             </div>
