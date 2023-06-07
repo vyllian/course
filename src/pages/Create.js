29cac493bea4ab9f05b8190b4923c52436060e43
vyllian <anastasiia.vylitkova.kn.2021@lpnu.ua>
2023-06-08 00:23:28 +0300

    imported clothes vectors

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -2,6 +2,13 @@ import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import download from "../media/download.svg";
+import hat from "../media/clothes_template/beret.svg"
+import coat from "../media/clothes_template/coat.svg"
+import tshirt from "../media/clothes_template/tshirt.svg"
+import jeans from "../media/clothes_template/jeans.svg"
+import glasses from "../media/clothes_template/glasses.svg"
+import bag from "../media/clothes_template/bag.svg"
+import shoes from "../media/clothes_template/shoes.svg"
 
 
 class Create extends React.Component{
@@ -17,7 +24,33 @@ class Create extends React.Component{
                     </div>
                    <div className="creating-container">
                         <div className="creating-template">
-
+                            <div className="template-section">
+                                <div className="item">
+                                    <img src={hat} alt="hat"></img>
+                                </div>
+                                <div className="item">
+                                    <img src={coat} alt="coat"></img>
+                                </div>
+                            </div>
+                            <div className="template-section">
+                                <div className="item">
+                                    <img src={tshirt} alt="t-shirt"></img>
+                                </div>
+                                <div className="item">
+                                    <img src={jeans} alt="jeans"></img>
+                                </div>
+                            </div>
+                            <div className="template-section">
+                                <div className="item">
+                                    <img src={glasses} alt="glasses"></img>
+                                </div>
+                                <div className="item">
+                                    <img src={bag} alt="bag"></img>
+                                </div>
+                                <div className="item">
+                                    <img src={shoes} alt="shoes"></img>
+                                </div>
+                            </div>
                         </div>
                         <div className="template-bar">
                             <button className="button-container create-button">Publish</button>
